5892fb319a6fd83eaa081ff77e9277807e61d8a0
Jose Simancas <josesimancas51@gmail.com>
2024-09-16 11:21:58 -0500

    feat: add selector to start filtering digimons by name or level

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,38 +1,53 @@
+"use client"
 import Image from "next/image";
+import { useState, useRef } from "react";
 
-const Navbar = () => (
-    <div className="navbar flex-col sm:flex-row">
-        <div className="flex-1">
-            <a className="btn">
-                <Image
-                    src="svg/the_digimon_logo.svg"
-                    alt="Digimon Wiki Logo"
-                    width={150}
-                    height={150}
-                />
-            </a>
+const Navbar = () => {
+    // choose search option
+    const [searchOption, setSearchOption] = useState<string>("");
+    //select reference
+    const selectRef = useRef<HTMLSelectElement>(null);
+    return (
+        <div className="navbar flex-col sm:flex-row">
+            <div className="flex-1">
+                <a className="btn">
+                    <Image
+                        src="svg/the_digimon_logo.svg"
+                        alt="Digimon Wiki Logo"
+                        width={150}
+                        height={150}
+                    />
+                </a>
+            </div>
+            <div className="flex-none mt-3 sm:mt-0">
+                <select
+                    onChange={() => setSearchOption(selectRef.current?.value ?? "")}
+                    ref={selectRef}
+                    className="select select-primary w-full max-w-xs mr-4"
+                    defaultValue=""
+                >
+                    <option disabled value="">Buscar Digimon Por:</option>
+                    <option value="name">Nombre:</option>
+                    <option value="level">Level:</option>
+                </select>
+                <label className="input input-bordered flex items-center gap-2 input-primary">
+                    <input disabled={!searchOption} type="text" className="grow" placeholder="Buscar" />
+                    <button role="button">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4 opacity-70">
+                            <path
+                                fillRule="evenodd"
+                                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </label>
+            </div>
         </div>
-        <div className="flex-none">
-            <h6 className="mr-6">
-                Digimon Favorito:
-            </h6>
-            <label className="input input-bordered flex items-center gap-2 input-primary">
-                <input type="text" className="grow" placeholder="Buscar" />
-                <button role="button">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-4 w-4 opacity-70">
-                        <path
-                            fillRule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clipRule="evenodd" />
-                    </svg>
-                </button>
-            </label>
-        </div>
-    </div>
-)
+    )
+}
 
 export default Navbar;
