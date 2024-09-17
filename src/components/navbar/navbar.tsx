ff6d7446b0a0bb1f0e651aa075bb52ba6baf28dc
Jose Simancas <josesimancas51@gmail.com>
2024-09-17 16:51:59 -0500

    Fix: digimones were being fetched twice in the initial load

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -21,7 +21,9 @@ const Navbar = () => {
 
     //it will listen to digimon level selector
     useEffect(() => {
-        router.push(`/?level=${digimonLevel}`)
+        if (digimonLevel) {
+            router.push(`/?level=${digimonLevel}`)
+        }
     }, [digimonLevel])
 
     return (
